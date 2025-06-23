f8bb0d4db18414a0c92c386d938e1e9707a70ad9
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-06-23 14:54:26 +0530

    React with Vite Complete Go through

--- 01Practise/src/App.jsx
+++ 01Practise/src/App.jsx
@@ -3,8 +3,16 @@
 // import './App.css'
 
 function App() {
+  const person = {
+      name : "John Doe",
+      age : 30,
+  }
   return (
+    <>
     <h1>CODE BY ME</h1>
+    {person.name }
+    </>
+
   )
 }
 
